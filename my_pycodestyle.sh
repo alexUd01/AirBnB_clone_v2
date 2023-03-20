7f2d557c4eb4d6a73deafcdda1dd49ccbb1598fe
alexUd01 <alexanderikpeama@gmail.com>
2023-03-20 20:51:34 +0100

    Added a few more changes

--- my_pycodestyle.sh
+++ my_pycodestyle.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # custom automator for pycodestyle check for the current project
 
-pycodestyle -v $(ls -R *.py ./*/*.py ./*/*/*.py | tr "\n" ' ')
+pycodestyle -v $(ls -R *.py ./*/*.py ./*/*/*.py ./*/*/*/*.py | tr "\n" ' ')
