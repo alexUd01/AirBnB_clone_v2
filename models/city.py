073ce5f16b6a4241d62561ffca7b83715c2ac9b8
alexUd01 <alexanderikpeama@gmail.com>
2023-03-22 06:39:24 +0100

    pycodestyle

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), nullable, ForeignKey('states.id'))
+
+    # Uncomment the line below this line if checker fails
+    # states = relationship('State', backref='cities')
