3cf54e39bef6af099bbc3b539fbc66accf9f1a68
alexUd01 <alexanderikpeama@gmail.com>
2023-04-25 01:06:35 +0100

    task 1

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -8,19 +8,19 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """Returns a greeting"""
-    return "Hello HBNB!"
+    return "Hello HBNB!\n"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Returns HBNB"""
-    return "HBNB"
+    return "HBNB\n"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """Returns something"""
-    return "C {}".format(text.replace('_', ' '))
+    return "C {}\n".format(text.replace('_', ' '))
 
 
 if __name__ == "__main__":
